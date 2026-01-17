21817b666302f2bbfac63984afda10624a8dbde6
Mohamed <moham@example.com>
2026-01-17 15:09:08 +0100

    Fix: Authenticate middleware for API-only project

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,6 +12,7 @@ class Authenticate extends Middleware
      */
     protected function redirectTo(Request $request): ?string
     {
-        return $request->expectsJson() ? null : route('login');
+        // This is an API-only project, always return null to get JSON 401 response
+        return null;
     }
 }
